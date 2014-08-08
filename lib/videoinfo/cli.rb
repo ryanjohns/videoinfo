082735eb4de76389929762a49d9c9818314ea92b
Ryan Johns <ryanjohns@gmail.com>
2014-08-08 03:41:06 -0700

    more flexible CLI when using a custom image host

--- lib/videoinfo/cli.rb
+++ lib/videoinfo/cli.rb
@@ -3,20 +3,17 @@ require 'optparse'
 module Videoinfo
   class CLI
 
+    ENV_IMAGE_HOST = 'VIDEOINFO_IMAGE_HOST'
+
     # Analyze a video from command line arguments and print the result to STDOUT.
     def self.run(args)
       Videoinfo.interactive = true
+      image_host            = ENV[ENV_IMAGE_HOST] || Videoinfo.image_host.class.to_s.split('::').last
       screenshots           = 2
       option_parser         = OptionParser.new do |opts|
         opts.banner = 'Usage: videoinfo [options] "MOVIENAME" file'
-        opts.on('-i', '--image-host=IMAGEHOST', "The ImageHost to use for uploading screenshots. Default: #{Videoinfo.image_host.class.to_s.split('::').last}") do |host|
-          begin
-            Videoinfo.image_host = Videoinfo::ImageHosts.const_get(host).new
-          rescue
-            hosts = Videoinfo::ImageHosts.constants.map(&:to_s).join(', ')
-            STDERR.puts "ERROR: '#{host}' is an unknown image host. Available hosts: #{hosts}"
-            exit 1
-          end
+        opts.on('-i', '--image-host=IMAGEHOST', "The ImageHost to use for uploading screenshots. Default: #{image_host}") do |host|
+          image_host = host
         end
         opts.on('-s', '--screenshots=SCREENSHOTS', "The number of screenshots to create, max 7. Default: #{screenshots}") do |ss|
           screenshots = ss.to_i
@@ -32,6 +29,14 @@ module Videoinfo
           puts opts
           exit
         end
+      end
+
+      begin
+        Videoinfo.image_host = Videoinfo::ImageHosts.const_get(image_host).new
+      rescue
+        hosts = Videoinfo::ImageHosts.constants.map(&:to_s).join(', ')
+        STDERR.puts "ERROR: '#{image_host}' is an unknown image host. Available hosts: #{hosts}"
+        exit 1
       end
 
       name, file = option_parser.parse!(args)
